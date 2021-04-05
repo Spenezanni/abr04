cc9a3318e379ba8ee31fe03cc403c1aa71dc2f89
Luiz Gustavo <gustavospenezanni@gmail.com>
2021-04-05 08:38:48 -0300

    init commit

---
+++ src/main/java/br/com/abr04/Abr04Application.java
@@ -0,0 +1,13 @@
+package br.com.abr04;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Abr04Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Abr04Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/abr04/Abr04ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.abr04;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Abr04ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
